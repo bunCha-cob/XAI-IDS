79d10ad1f6f0cd060c50f989a7a3b5a99f04e032
bunCha-cob <52370107+bunCha-cob@users.noreply.github.com>
2021-10-10 14:45:08 +1100

    update codes in second technical chapter

--- ToN_IoT/DNN_TTN.py
+++ ToN_IoT/DNN_TTN.py
@@ -5,140 +5,148 @@ import pandas as pd
 
 # import dataset
 dataset = pd.read_csv('Train_Test_Network.csv')
-print(dataset.columns)
+
+shap_features = ['proto', 'conn_state', 'dns_AA', 'dns_RD', 'dns_qtype', 'service', 'dns_rejected', 'dns_query', 
+                 'dns_rcode', 'dns_RA', 'dns_qclass', 'http_version', 'weird_notice', 'http_user_agent', 
+                 'weird_addl', 'ssl_resumed', 'ssl_established', 'http_status_code', 'http_uri', 
+                 'http_resp_mime_types']
+
+pfi_features = ['proto', 'conn_state', 'dns_AA', 'service', 'dns_RD', 'dns_query', 'dns_qtype', 
+                'dns_rejected', 'dns_rcode', 'dns_RA', 'dns_qclass', 'weird_notice', 'ssl_resumed',
+                 'http_version', 'ssl_established', 'weird_addl', 'weird_name', 'ssl_version', 
+                 'http_uri', 'src_bytes']
+
+new = set(pfi_features) - set(shap_features)
+
+combine_features = shap_features + list(new)
+
+print(combine_features)
+
+#%% 
+# binary classifier
+y = (dataset.label != 0).astype(int)
+y = y.to_frame()
+
+features = [c for c in dataset.columns if (c != "label") and (c != "type")]
+X = dataset[features]
+
+# delete irrelevant features
+list_delete = ['ts','src_ip', 'src_port', 'dst_ip', 'dst_port']
+X = X.drop(columns = list_delete)
 
 #%%
-X = dataset.iloc[:, :-2].values
-y = dataset.iloc[:,-2].values
-
 # label encoding data
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
-list_encode = [0, 1, 3, 5, 6, 10, 16, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 37, 38, 39, 40, 41, 42]
 
-for i in range(len(list_encode)):
-    X[:, list_encode[i]] = le.fit_transform(X[:, list_encode[i]])
+list_encode = ['proto', 'service', 'conn_state', 'dns_query', 'dns_AA', 'dns_RD', 'dns_RA', 'dns_rejected', 
+               'ssl_version',  'ssl_cipher', 'ssl_resumed', 'ssl_established', 'ssl_subject', 
+               'ssl_issuer', 'http_method', 'http_uri', 'http_version', 'http_user_agent' , 
+               'http_orig_mime_types', 'http_resp_mime_types', 'weird_name', 'weird_addl',
+               'weird_notice', 'http_trans_depth']
+
+for i in list_encode:
+    X[i] = le.fit_transform(X[i])
     
-X = np.delete(X, 0, 1)
-X = np.delete(X, 1, 1)
-X = np.delete(X, 2, 1)
-X = np.delete(X, 3, 1)
-X = np.delete(X, 4, 1)
-X = np.delete(X, 5, 1)   
-    
+#%%
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
 
+#%%
 # feature scaling
-from sklearn.preprocessing import Normalizer
-scaler = Normalizer().fit(X_train)
-X_train = scaler.transform(X_train)
-scaler = Normalizer().fit(X_test)
-X_test = scaler.transform(X_test)
-val_X = X_test
-val_y = y_test
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
 
-
-# reformat
-y_train = y_train.astype(np.int64)
-y_test = y_test.astype(np.int64)
-
-#%%
-# build the model
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation
-from tensorflow.keras import optimizers
-
-# settings for the model 
-opt = optimizers.Adam(lr = 0.001) # optimizer
-num_nodes = 50  # number of hidden nodes
-b_size = 32 # batch size
-ep = 10 # epoch
-dropout_rate = 0.1  
-
-model = Sequential()
-model.add(Dense(num_nodes,input_dim=37, activation='relu'))  
-model.add(Dropout(dropout_rate))
-model.add(Dense(num_nodes, activation='relu'))  
-model.add(Dropout(dropout_rate))
-model.add(Dense(num_nodes, activation='relu'))  
-model.add(Dropout(dropout_rate))
-model.add(Dense(num_nodes, activation='relu'))  
-model.add(Dropout(dropout_rate))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-
-# train the model
-model.compile(loss='binary_crossentropy', optimizer = opt, metrics = ['accuracy'])
-history = model.fit(X_train, y_train, epochs = ep, batch_size = b_size, validation_split=0.1, verbose=1)
-score = model.evaluate(X_test, y_test, batch_size = b_size)
-
-from tensorflow.keras.models import model_from_json
-# serialise model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialise weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
+X_train.iloc[:,:] = scaler.fit_transform(X_train.to_numpy())
+X_test.iloc[:,:] = scaler.transform(X_test.to_numpy())
 
 #%%
 from tensorflow.keras.models import model_from_json
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
+from time import time
+
+# load json and create model for ddos
+json_file = open('model_ddos_tvn.json', 'r')
+loaded_model_ddos_json = json_file.read()
 json_file.close()
-loaded_model = model_from_json(loaded_model_json)
+loaded_model_ddos = model_from_json(loaded_model_ddos_json)
 # load weights into new model 
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
+loaded_model_ddos.load_weights("model_ddos_tvn.h5")
+print("Loaded model_ddos from disk")
 
-# predict test set resutl and make confusion matrix
-y_pred = loaded_model.predict(X_test)
-y_pred_binary = (y_pred > 0.5)
+# load json and create model for xss
+json_file = open('model_xss_tvn.json', 'r')
+loaded_model_xss_json = json_file.read()
+json_file.close()
+loaded_model_xss = model_from_json(loaded_model_xss_json)
+# load weights into new model
+loaded_model_xss.load_weights('model_xss_tvn.h5')
+print("Loaded model_xss from disk")
+
+# load json and create model for other attacks
+json_file = open('model_binary.json', 'r')
+loaded_model_binary_json = json_file.read()
+json_file.close()
+loaded_model_binary = model_from_json(loaded_model_binary_json)
+# load weights into new model
+loaded_model_binary.load_weights('model_binary.h5')
+print("Loaded model_binary from disk")
+
+# customized set of features for X_test
+X_test_cf = X_test[combine_features]
+
+# predict test set result 
+# calculate processing time
+start = int(time() * 1000) 
+y_pred_binary = loaded_model_binary.predict(X_test_cf)
+y_pred_ddos = loaded_model_ddos.predict(X_test_cf)
+y_pred_xss = loaded_model_xss.predict(X_test_cf)
+stop = int(time() * 1000) 
+print ("Processing time = ", stop - start)
+
+#%%
+y_pred_ddos = (y_pred_ddos > 0.5)
+y_pred_xss = (y_pred_xss > 0.5)
+y_pred_binary = (y_pred_binary > 0.5)
+
+y_pred = [None] * len(y_pred_ddos)
+for i in range(len(y_pred_ddos)):
+    if (y_pred_ddos[i]==True) or (y_pred_xss[i]==True) or (y_pred_binary[i]==True):
+        y_pred[i]=True
+    else: 
+        y_pred[i]=False
+    
+#%%
+# make confusion matrix
 from sklearn.metrics import confusion_matrix
-
-def get_confusion_matrix_values(z_true, z_pred):
-    cm = confusion_matrix(z_true, z_pred)
-    return(cm[0][0], cm[0][1], cm[1][0], cm[1][1])
-
-TP, FP, FN, TN = get_confusion_matrix_values(y_test, y_pred_binary)
-detection_rate = TP/(TP+FN)
-false_alarm_rate = FP/(TN+FP)
+TN, FP, FN, TP = confusion_matrix(y_test, y_pred).ravel()
 accuracy = (TP+TN)/(TP+FP+TN+FN)
 precision = TP/(TP+FP)
 recall = TP/(TP+FN)
 F1 = (2*precision*recall)/(precision + recall)
 
-print("\n Detection rate = ", detection_rate, "\n")
-print("False alarm rate = ", false_alarm_rate, "\n")
+print("TN = ", TN," FP = ", FP, " FN = ", FN, " TP = ", TP, "\n")
+
 print("Accuracy = ", accuracy, "\n")
 print("Precision = ", precision, "\n")
 print("Recall = ", recall, "\n")
 print("F1 = ", F1, "\n")
 
 #%%
-from matplotlib import pyplot as plt
-from pdpbox import pdp, get_dataset, info_plots
-import pandas as pd
+import shap  # package used to calculate Shap values
+import numpy as np
 
-# Create the data that we will plot
-pdp_goals = pdp.pdp_isolate(model=loaded_model, dataset=pd.DataFrame(X_test, columns=[*range(0,37,1)]), 
-                            model_features=[*range(0,37,1)], feature=7)
-
-# plot it
-pdp.pdp_plot(pdp_goals, 'feature 7')
-plt.show()
+sample = X_test.sample(n = 5000)
+explainer = shap.DeepExplainer(loaded_model_ddos, np.array(sample))
+shap_values = explainer.shap_values(np.array(sample))
 
 #%%
-import shap
 
-sample = X_test[np.random.choice(X_test.shape[0], 2000, replace=False)]
- 
-explainer = shap.DeepExplainer(loaded_model,sample)
-shap_values = explainer.shap_values(sample)
+index = 925
+row_to_show = X_test.iloc[index:index+1]
 
+shap_values = explainer.shap_values(np.array(row_to_show))
 shap.initjs()
-force_plot = shap.force_plot(explainer.expected_value[0].numpy().tolist(), shap_values[0], sample)
-summary_plot = shap.summary_plot(shap_values[0], sample)
+shap.force_plot(explainer.expected_value[0].numpy().tolist(), shap_values[0], row_to_show) 
+
+
